ad8c2d3f50ffe1d3392e9fd669439a9fc608a82f
benmc1 <benmc2002@gmail.com>
2022-04-06 13:59:29 +0100

    Structs

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 //
 // Created by benmc on 06/04/2022.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "main.h"
+typedef struct  {
+    char username[15];
+    unsigned int numFollowers;
+    unsigned int numFollowing;
+    struct User *Followers[20];
+    struct User *Following[20];
+}User ;
+typedef struct {
+    char author[15];
+    char text[280];
+    struct tweet *nextTwt;
+}tweet;
+
+typedef struct {
+    User allUsers[20];
+    int numUsers;
+    tweet *firstTwt;
+}TwitterSys;
+int main(){
+
+}
